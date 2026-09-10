9c59e940b765b6ed0c00ce767c83c3a59ba74507
wik <81856716+wikmatsurg@users.noreply.github.com>
2026-09-10 17:32:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let unixTarget = 1789064610
 window.addEventListener('load', function() {
   setInterval(function() {
-    let unixNow = Math.floor(Date.now())
+    let unixNow = Math.floor(Date.now()/1000)
     let counter = document.getElementById("counter")
     counter.innerHTML = (unixTarget - unixNow)
   }, 1000)
