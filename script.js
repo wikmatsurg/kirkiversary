925dc54f769be25aee968d5a4ceb7c98504744ea
wik <81856716+wikmatsurg@users.noreply.github.com>
2026-09-10 17:01:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let unixTarget = 1789064610
-setInterval(function() {
-  let unixNow = Math.floor(Date.now())
-  let counter = document.getElementById("counter")
-  counter.innerHTML = unixTarget - unixNow
-}, 1000)
+window.onload(function() {
+  setInterval(function() {
+    let unixNow = Math.floor(Date.now())
+    let counter = document.getElementById("counter")
+    counter.innerHTML = unixTarget - unixNow
+  }, 1000)
+})
+
 
