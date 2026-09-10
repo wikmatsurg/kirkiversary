e2780df77dad72fb48fab5738a8a78bfa1a34daa
wik <81856716+wikmatsurg@users.noreply.github.com>
2026-09-10 19:18:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let unixTarget = 1789064610
 window.addEventListener('load', function() {
+  let kirk = document.getElementById("kirk")
+  kirk.play()
   setInterval(function() {
     let unixNow = Math.floor(Date.now()/1000)
     let counter = document.getElementById("counter")
