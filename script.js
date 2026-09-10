8488b83118ae9aa1239fa58d30a9a349d1e7a0db
wik <81856716+wikmatsurg@users.noreply.github.com>
2026-09-10 17:41:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ window.addEventListener('load', function() {
   setInterval(function() {
     let unixNow = Math.floor(Date.now()/1000)
     let counter = document.getElementById("counter")
-    counter.innerHTML = (unixTarget - unixNow)
+    let secs = (unixTarget - unixNow)
+    let days = secs % 86400
+    secs -= days*86400
+    let hours = secs % 3600
+    secs -= days*3600
+    let mins = secs % 60
+    secs -= days*60
+    counter.innerHTML = (days+" days, "+hours+" hours, "+mins+" minutes and "+secs+" seconds left.")
   }, 1000)
 });
   
